638113cd6ebba98c1d95f8984e94b6df4ddcb936
Renan Greca <renangreca@gmail.com>
2018-06-03 12:03:28 -0300

    Prepared layout for first three sections

---
+++ themes/luizagreca/header.php
@@ -0,0 +1,45 @@
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0,
+minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+
+  <link href="<?php echo get_bloginfo('template_url'); ?>/style.css" rel="stylesheet">
+  <link href="<?php echo get_bloginfo('template_url'); ?>/css/header.css" rel="stylesheet">
+
+  <title>
+    Luiza Greca
+  </title>
+
+</head>
+
+  <div class="container">
+    <div class="navbar">
+
+      <div class="brand">
+        <a class="brand-link" href="<?php echo site_url(); ?>">
+          <img class="logo" src="<?php echo get_bloginfo('template_url') ?>/img/symbol.png" />
+          <!-- <span class="logo-text">NINTENDO FUSION</span> -->
+        </a>
+      </div>
+
+      <div class="nav-collapse collapse">
+        <ul class="nav">
+            <li><a class="navbar-link" href="#casos">CASOS</a></li>
+            <li><a class="navbar-link" href="#noticias">NOTÍCIAS</a></li>
+            <li><a class="navbar-link" href="#atuacao">ÁREAS DE ATUAÇÃO</a></li>
+            <li><a class="navbar-link" href="#sobre">SOBRE</a></li>
+            <li><a class="navbar-link" href="#contato">CONTATO</a></li>
+
+
+            <li><a href="https://facebook.com/" class="social-icon social-fb" target="_blank">
+              <img class="social-icon" src="<?php echo get_bloginfo('template_url') ?>/img/facebook.png">
+            </a></li>
+
+        </ul>
+
+      </div>
+
+    </div>
+
+  </div>
---
+++ themes/luizagreca/home.php
@@ -0,0 +1,124 @@
+<?php get_header(); ?>
+
+
+<div class="part1">
+
+  <div class="gradient-white-bottom">
+  </div>
+
+  <span class="logo-helper"></span>
+  <img class="main-logo" src="<?php echo get_bloginfo('template_url') ?>/img/logo-h.png"/>
+
+</div>
+
+<span class="horizontal-line"></span>
+<div class="container">
+  <div id="casos" class="section">
+    <h1>Casos em Destaque</h1>
+
+    <div class="mosaic-row">
+
+      <div class="mosaic-item">
+        <div class="mosaic-image">
+        </div>
+        <h2>Título do caso</h2>
+        Breve descrição do caso relatado na matéria. No máximo três linhas.
+      </div>
+
+      <div class="mosaic-item">
+        <div class="mosaic-image">
+        </div>
+        <h2>Título do caso maior que o normal</h2>
+        Breve descrição do caso relatado na matéria. No máximo três linhas.
+      </div>
+
+      <div class="mosaic-item">
+        <div class="mosaic-image">
+        </div>
+        <h2>Título do caso</h2>
+        Breve descrição do caso relatado na matéria. No máximo três linhas.
+      </div>
+
+    </div>
+
+  </div>
+
+</div>
+<span class="horizontal-line line-right"></span>
+<div class="container">
+<a class="standard-link" href="/casos"><h3>
+    Leia outros casos aqui
+</h3></a>
+</div>
+
+<span class="horizontal-line"></span>
+<div class="container">
+  <div id="noticias" class="section">
+    <h1>Últimas Notícias</h1>
+
+    <div class="mosaic-row">
+
+      <div class="mosaic-item">
+        <div class="mosaic-image">
+        </div>
+        <h2>Título da notícia</h2>
+        Breve descrição da notícia relatada na matéria. No máximo três linhas.
+      </div>
+
+      <div class="mosaic-item">
+        <div class="mosaic-image">
+        </div>
+        <h2>Título da notícia maior que o normal</h2>
+        Breve descrição da notícia relatada na matéria. No máximo três linhas. Esta descrição é maior que as outras.
+      </div>
+
+      <div class="mosaic-item">
+        <div class="mosaic-image">
+        </div>
+        <h2>Título da notícia</h2>
+        Breve descrição da notícia relatada na matéria. No máximo três linhas.
+      </div>
+
+    </div>
+
+  </div>
+</div>
+<span class="horizontal-line line-right"></span>
+<div class="container">
+<a class="standard-link" href="/noticias"><h3>
+  Leia outras notícias aqui
+</h3></a>
+</div>
+
+<div class="atuacao-bg">
+
+  <div class="gradient-white-top">
+  </div>
+
+  <div class="gradient-white-bottom">
+  </div>
+
+  <span class="horizontal-line"></span>
+  <div class="container">
+    <div id="atuacao" class="section">
+      <h1>Áreas de Atuação</h1>
+
+    </div>
+  </div>
+</div>
+
+<span class="horizontal-line"></span>
+<div class="container">
+  <div class="casos">
+    <h1>Sobre</h1>
+  </div>
+</div>
+
+<span class="horizontal-line"></span>
+<div class="container">
+  <div class="casos">
+    <h1>Contato</h1>
+  </div>
+</div>
+
+<?php get_footer(); ?>
